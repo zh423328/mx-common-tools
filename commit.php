a06cca2fbbdf6389d40f57fbf4e6e37e0f2ef31b
Go-Road <582837437@qq.com>
2022-09-15 15:47:44 +0800

    进行细微的调整,添加了curl post/get

--- commit.php
+++ commit.php
@@ -6,5 +6,5 @@ $msg = '进行细微的调整,添加了curl post/get';
 echo `git add .`;
 echo `git commit -m "$msg"`;
 echo `git tag $tag`;
-echo `git push origin master`;
+echo `git push origin main`;
 echo `git push origin --tags`;
